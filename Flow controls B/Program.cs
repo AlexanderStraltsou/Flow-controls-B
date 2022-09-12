c5ab36fc5f8964ec96fcaf9dc109d44f8f0f8f34
AlexanderStraltsou <113332203+AlexanderStraltsou@users.noreply.github.com>
2022-09-12 14:47:44 +0200

    Initial commit

---
+++ Flow controls B/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Type in a random number!");
+int x = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Type in a random amount!");
+int y = Convert.ToInt32(Console.ReadLine());
+
+for (int i = 0; i < y; i++)
+{
+    Console.WriteLine("-------");
+    Console.WriteLine();
+    Console.WriteLine(x);
+}
