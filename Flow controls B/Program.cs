87fed773213ab41c2943bd744ecfe2dc97870801
AlexanderStraltsou <113332203+AlexanderStraltsou@users.noreply.github.com>
2022-09-12 14:49:36 +0200

    Monster Battle Game
    Monster Battle Game

--- Flow controls B/Program.cs
+++ Flow controls B/Program.cs
@@ -1,4 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿/*See https://aka.ms/new-console-template for more information
 Console.WriteLine("Type in a random number!");
 int x = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Type in a random amount!");
@@ -10,3 +10,6 @@ for (int i = 0; i < y; i++)
     Console.WriteLine();
     Console.WriteLine(x);
 }
+*/
+
+
